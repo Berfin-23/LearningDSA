602187e777dd6dec6933ad8485c9eb88b99df067
Berbin MS <berfinms@icloud.com>
2025-03-14 15:00:15 +0530

    Add StringMethod class with examples of string manipulation methods and add BubbleSort class for sorting an array

---
+++ BubbleSort/BubbleSort.java
@@ -0,0 +1,20 @@
+class BubbleSort {
+    public static void main(String args[]){
+        int[] arr = {5, 1, 4, 2, 8};
+        int temp;
+        
+        for(int i = 0; i < arr.length - 1; i++){
+            for(int j = 0; j < arr.length - i - 1; j++){
+                if(arr[j] > arr[j+1]){
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        System.out.println("Sorted array:");
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
--- StringMethod/StringMethod.java
+++ StringMethod/StringMethod.java
@@ -1,24 +1,50 @@
-class StringMethods {
+class StringMethod {
     public static void main(String[] args) {
+        
+        // 1) length() method
+        // The length() method returns the number of characters in the string.
+        // It returns an int value.
+        // Cannot count emojis
         String str = "Hello, World!";
-        System.out.println("Original String: " + str);
-        System.out.println("Length of String: " + str.length());
-        System.out.println("Substring(0, 5): " + str.substring(0, 5));
-        System.out.println("Substring(7, 12): " + str.substring(7, 12));
-        System.out.println("Substring(7): " + str.substring(7));
-        System.out.println("Index of 'o': " + str.indexOf('o'));
-        System.out.println("Index of 'o' after 5: " + str.indexOf('o', 5));
-        System.out.println("Index of 'o' after 8: " + str.indexOf('o', 8));
-        System.out.println("Last Index of 'o': " + str.lastIndexOf('o'));
-        System.out.println("Last Index of 'o' before 5: " + str.lastIndexOf('o', 5));
-        System.out.println("Last Index of 'o' before 8: " + str.lastIndexOf('o', 8));
-        System.out.println("Character at 7: " + str.charAt(7));
-        System.out.println("Character at 8: " + str.charAt(8));
-        System.out.println("Concatenation: " + str.concat(" Goodbye!"));
-        System.out.println("Replace: " + str.replace('o', 'x'));
-        System.out.println("Replace: " + str.replace("World", "Universe"));
-        System.out.println("Lowercase: " + str.toLowerCase());
-        System.out.println("Uppercase: " + str.toUpperCase());
-        System.out.println("Trim: " + "  Hello, World!  ".trim());
+        int length = str.length(); 
+        System.out.println("THe length of the string is " + length);
+
+        // 2) matches() method
+        // The matches() method checks if the string matches a given regular expression.
+        // It returns a boolean value.
+        // The regular expression is a sequence of characters that forms a search pattern.
+        String email = "example@gmail.com";
+        String regex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+        boolean isValidEmail = email.matches(regex);
+        System.out.println(isValidEmail ? "Valid email address" : "Invalid email address");
+
+        // 3) offsetByCodePoints() method
+        // The offsetByCodePoints() method returns the index within this string that is offset from the given index by the specified code point offset.
+        // It returns an int value.
+        // The code point is a number that represents a character in the Unicode standard.
+        String str2 = "Hell❤️, World!";
+        int index = 7;
+        int codePointOffset = 2;
+        System.out.println(str2.charAt(5));
+        int newIndex = str2.offsetByCodePoints(index, codePointOffset);
+        System.out.println("The new index is " + newIndex);
+
+
+        // 4) regionMatches() method
+        // The regionMatches() method compares a specified region of this string to a specified region of another string.
+        // It returns a boolean value.
+        // Parameters
+        // boolean ignoreCase: if true, ignore case differences
+        // int firstOffset: the starting offset of the region in this string
+        // int secondOffset: the starting offset of the region in the other string
+        // int len: the number of characters to compare
+        // The regionMatches() method is case-sensitive by default.
+        String string = "Hello, World!";
+        String otherString = "world";
+        int firstOffset = 7;
+        int secondOffset = 0;
+        int len = 5;
+        boolean isEqual = string.regionMatches(true, firstOffset, otherString, secondOffset, len);
+        System.out.println(isEqual ? "The regions are equal" : "The regions are not equal");
     }
 }
